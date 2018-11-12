218b3095b7254dd656cd658463376bd713758d37
Yuval Pinter <uvp@gatech.edu>
2018-11-12 17:04:46 -0500

    support for more eval modes

--- static_att_analysis.py
+++ static_att_analysis.py
@@ -9,13 +9,14 @@ from utils import split_tagstring
 from evaluate_morphotags import Evaluator
 from prop_weigh import sqrt_w, log_w, inv_w, linear_w, sum_w, sq_w, rand_w
 
-def read_results_file(in_file):
+def read_results_file(in_file, attval):
     """
     :return: list of {gold tags, observed tags} tuples, and an Evaluator object
     """
     golds = []
     obsvs = []
-    ev = Evaluator()
+    mode = 'att_val' if attval else 'att'
+    ev = Evaluator(m=mode)
     gold_insts = 0
     errs = Counter()
     with open(in_file) as in_lines:
@@ -40,16 +41,33 @@ def read_results_file(in_file):
 
 def err_fix(err_type, ev):
     att, gt, ot = err_type
-    if gt == None:
-        # false positive
-        ev.observed[att] -= 1
-    elif ot == None:
-        # false negative
-        ev.observed[att] += 1
-        ev.correct[att] += 1
+    assert gt != ot
+    if ev.mode == 'att':
+        if gt == None:
+            # false positive
+            ev.observed[att] -= 1
+        elif ot == None:
+            # false negative
+            ev.observed[att] += 1
+            ev.correct[att] += 1
+        else:
+            # misclassification
+            ev.correct[att] += 1
+    elif ev.mode == 'att_val':
+        if gt == None:
+            # false positive
+            ev.observed[(att, ot)] -= 1
+        elif ot == None:
+            # false negative
+            ev.observed[(att, gt)] += 1
+            ev.correct[(att, gt)] += 1
+        else:
+            # misclassification
+            ev.observed[(att, ot)] -= 1
+            ev.observed[(att, gt)] += 1
+            ev.correct[(att, gt)] += 1
     else:
-        # misclassification
-        ev.correct[att] += 1
+        raise Exception('Evaluation mode not supported')
 
 def agg_inc_check(errs, ev, weigh):
     """
@@ -67,7 +85,7 @@ def agg_inc_check(errs, ev, weigh):
         
         # calculate score and replace max, argmax, if winner
         ev2 = deepcopy(ev)
-        err_fix(tp,ev2)
+        err_fix(tp, ev2)
         score = ev2.mac_f1(weigh=wg)
         if score > max_score:
             max_score = score
@@ -97,6 +115,8 @@ def thorough_inc_check(exs, ev):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--infile")
+parser.add_argument("--outfile", default=None)
+parser.add_argument("--attval", action='store_true', help="use macro of <attribute, value> pairs")
 parser.add_argument("--fixes", type=int, default=10, help="number of fixing iterations to run, default - 10")
 parser.add_argument("--weighting", default='sum', help="weighting scheme. supported - log, sqrt, sum (macro, default), linear (micro), inv, sq, rand")
 parser.add_argument("--v", type=int, default=0, help="verbosity, range 0-3 (default 0)")
@@ -104,7 +124,9 @@ options = parser.parse_args()
 
 if options.v > 0:
     print 'reading from file', options.infile
-exs, ev, gold_insts, errs = read_results_file(options.infile)
+    if options.attval:
+        print 'using attval mode'
+exs, ev, gold_insts, errs = read_results_file(options.infile, options.attval)
 if options.v > 0:
     print 'read', len(exs[0]), 'instances with', gold_insts, 'tags and', sum(errs.values()), 'prediction errors across', len(errs), 'error types'
     if options.v > 2:
@@ -135,15 +157,23 @@ print 'basic micro F1:', ev.mic_f1()
 
 error_improvements = []
 tieses = []
-fixes_left = options.fixes
+fixes_left = min(options.fixes, sum(errs.values()))
 if options.v > 0:
     print 'performing', fixes_left, 'fixes'
-while sum(errs.values()) > 0 and fixes_left >= 0:
+    if options.outfile is not None:
+        print 'writing decisions to file', options.outfile
+        out_writer = open(options.outfile, 'w')
+        out_writer.write('att\tgold\tobserved\tnew f1\tties\n')
+while fixes_left >= 0:
     impr, ties = agg_inc_check(errs, ev, weigh=wg)
     error_improvements.append(impr)
     tieses.append(ties)
     if options.v > 1:
-        print impr, '\t', ev.mac_f1(weigh=wg), '\t', ties
+        att, g, o = impr
+        print '{}\t{}\t{}\t{:.5f}\t{}'.format(att, g, o, ev.mac_f1(weigh=wg), ties)
+    if options.outfile is not None:
+        att, g, o = impr
+        out_writer.write('{}\t{}\t{}\t{:.5f}\t{}\n'.format(att, g, o, ev.mac_f1(weigh=wg), ties))
     fixes_left -= 1
 
 print '==========================='
